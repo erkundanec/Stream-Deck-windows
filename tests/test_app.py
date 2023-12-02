9389ee632773bcf035785b7b4855d81427394cd3
Manish <139921211+manish-jsx@users.noreply.github.com>
2023-12-02 23:53:07 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,7 @@ from flask import Flask, url_for
 class StreamDeckAppTestCase(unittest.TestCase):
 
     def setUp(self):
+        # Create a test Flask app
         self.app = Flask(__name__)
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
@@ -14,9 +15,12 @@ class StreamDeckAppTestCase(unittest.TestCase):
         self.assertIn(b'Stream Deck', response.data)
 
     def test_static_file(self):
-        response = self.client.get(url_for('static', filename='styles.css'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'body', response.data)
+        # Set up a test request context
+        with self.app.test_request_context():
+            response = self.client.get(url_for('static', filename='styles.css'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'body', response.data)
 
 if __name__ == '__main__':
     unittest.main()
+
