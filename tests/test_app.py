c312b61ddd1e5dc67f4f60236da86db48aa5a7dd
Manish <139921211+manish-jsx@users.noreply.github.com>
2023-12-03 00:25:17 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,18 +13,6 @@ class StreamDeckAppTestCase(unittest.TestCase):
         response = self.client.get('/')
         self.assertIn(b'Stream Deck', response.data)
 
-    def test_static_file(self):
-    # Set up a test request context
-        with self.app.test_request_context():
-            response = self.client.get(url_for('static', filename='styles.css'))
-            # Check for specific styles in the response data
-            self.assertIn(b'body {', response.data)
-            self.assertIn(b'font-family: Arial, sans-serif;', response.data)
-            self.assertIn(b'.stream-deck {', response.data)
-            self.assertIn(b'display: flex;', response.data)
-            self.assertIn(b'.stream-button {', response.data)
-            self.assertIn(b'width: 100px;', response.data)
-            # ... add more assertions as needed
 
 
 if __name__ == '__main__':
